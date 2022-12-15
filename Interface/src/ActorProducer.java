460ed37660e44b8e830f4bcb1a8e87e5bf01f3a7
AdityaPrasetyoYogatama <101713148+AdityaPrasetyoYogatama@users.noreply.github.com>
2022-12-15 08:19:02 +0700

    Add files via upload

---
+++ Interface/src/ActorProducer.java
@@ -0,0 +1,11 @@
+public class ActorProducer implements Actor, Producer{
+    @Override
+    public void perform() {
+        System.out.println("Implement perform() method for ActorProducer");
+    }
+
+    @Override
+    public void invest() {
+        System.out.println("Implement invest() method for ActorProducer");
+    }
+}
---
+++ Interface/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        ActorProducer ap = new ActorProducer();
+
+        ap.perform();
+        ap.invest();
+    }
+}
